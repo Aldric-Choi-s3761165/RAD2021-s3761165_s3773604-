6dcd4aeb99bc7dc0b2a5bf89237b018abf7c02d6
Aldric-Choi-s3761165 <s3761165@student.rmit.edu.au>
2021-05-11 07:19:55 +0000

    deleted the javascript starIcon

--- app/assets/javascripts/destroy.js
+++
@@ -1,4 +0,0 @@
-
-let starIcon = document.querySelector("#product_<%= @product.id %>").querySelector('.fa-star')
-
-starIcon.parentElement.outerHTML = "<%= escape_javascript(toggle_favorite(@product)) %>"
