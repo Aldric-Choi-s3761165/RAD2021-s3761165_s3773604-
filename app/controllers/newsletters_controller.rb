5df384cebfe5de2b83889515a51f510f6f172acc
Aldric-Choi-s3761165 <s3761165@student.rmit.edu.au>
2021-05-12 04:08:09 +0000

    changed more to mailer

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -8,7 +8,7 @@ class NewslettersController < ApplicationController
     @newsletter = Newsletter.new(newsletter_params)
     if @newsletter.save
       NewsletterNotifierMailer.send_subscribe_email(@newsletter).deliver_now
-      redirect_to '/newsletters/new'
+      redirect_to '/'
     else
       loadhomepagedata
       render 'static_pages/home'
