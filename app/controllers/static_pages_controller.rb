6f62037de11cbd444185397c9669a84b9d19cc0c
Aldric-Choi-s3761165 <s3761165@student.rmit.edu.au>
2021-05-20 22:23:20 +0000

    uncommented newsletter

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     # @product = Product.find(Product.pluck(:id).sample)
     loadhomepagedata
-    # @newsletter = Newsletter.new
+    @newsletter = Newsletter.new
   end
 
   def helpandsupport
